dbf473149b5f32b413c55f67a101a7c67a18894e
Mark Bishop <mgrandyb@gmail.com>
2025-03-10 10:28:16 +0000

    bump version

--- src/goxel.h
+++ src/goxel.h
@@ -74,7 +74,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define GOXEL_VERSION_STR "0.13.0-aos-0_2l-pre3"
+#define GOXEL_VERSION_STR "0.13.0-aos-0_2l-pre4"
 #ifndef GOXEL_DEFAULT_THEME
 #   define GOXEL_DEFAULT_THEME "dark"
 #endif
