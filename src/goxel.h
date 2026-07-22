24b675b0d2fb3686c250d39e69d60a80fffe889b
Mark Bishop <mgrandyb@gmail.com>
2026-07-22 16:54:17 +0100

    2q version bump

--- src/goxel.h
+++ src/goxel.h
@@ -75,7 +75,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define GOXEL_VERSION_STR "0.13.0-aos-0_2p"
+#define GOXEL_VERSION_STR "0.13.0-aos-0_2q"
 #ifndef GOXEL_DEFAULT_THEME
 #   define GOXEL_DEFAULT_THEME "dark"
 #endif
