e6ee8aa1b4a3cadc80c3d834dff189035796cbce
Mark Bishop <mgrandyb@gmail.com>
2026-08-08 13:02:43 +0100

    version bump 0_3d

--- src/goxel.h
+++ src/goxel.h
@@ -79,7 +79,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define GOXEL_VERSION_STR "0.13.0-aos-0_3c"
+#define GOXEL_VERSION_STR "0.13.0-aos-0_3d"
 #ifndef GOXEL_DEFAULT_THEME
 #   define GOXEL_DEFAULT_THEME "dark"
 #endif
